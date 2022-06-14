93a87bd76392dcd701f5d2cd7471f835fa7b28ad
jadex_Desktop <jadexb04@gmail.com>
2022-06-14 00:06:12 -0500

    Preparando el entorno con la creacion de los entornos virtuales y su posterior activacion, tambien instalamos la version 1.11.7 de DJANGO

---
+++ DJANGO-EDTEAM/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Jeferson\Desktop\Proyectos\DJANGO_Desde_Cero\DJANGO-EDTEAM\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
